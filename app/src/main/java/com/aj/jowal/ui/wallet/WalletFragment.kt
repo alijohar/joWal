876bd695345743168686e87d8992614c7e2d1555
ali johar <johar.ali1@gmail.com>
2021-12-10 00:58:43 +0330

    Add binding data to editText

--- app/src/main/java/com/aj/jowal/ui/wallet/WalletFragment.kt
+++ app/src/main/java/com/aj/jowal/ui/wallet/WalletFragment.kt
@@ -4,38 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.aj.jowal.R
 import com.aj.jowal.databinding.FragmentWalletBinding
 
 class WalletFragment : Fragment() {
 
-    private lateinit var walletViewModel: WalletViewModel
-    private var _binding: FragmentWalletBinding? = null
-
+    private lateinit var viewModel: WalletViewModel
+    private var binding: FragmentWalletBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        walletViewModel =
-            ViewModelProvider(this).get(WalletViewModel::class.java)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(
+                inflater, R.layout.fragment_wallet, container, false
+            )
+        viewModel = ViewModelProvider(this)[WalletViewModel::class.java]
+        binding!!.viewModel = viewModel
+        binding!!.lifecycleOwner = this
 
-        _binding = FragmentWalletBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        return root
+        viewModel.cardNumber.observe(viewLifecycleOwner, {
+            viewModel.shibaCard.value = "IR 0445 0404 5454 $it"
+        })
+        return binding!!.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
--- app/src/main/java/com/aj/jowal/ui/wallet/WalletViewModel.kt
+++ app/src/main/java/com/aj/jowal/ui/wallet/WalletViewModel.kt
@@ -1,13 +1,19 @@
 package com.aj.jowal.ui.wallet
 
+import android.text.Editable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WalletViewModel : ViewModel() {
+    val cardNumber = MutableLiveData<String>()
+    val cardName = MutableLiveData<String>()
+    val expireDate = MutableLiveData<String>()
+    val shibaCard = MutableLiveData<String>()
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is Wallet Fragment"
     }
     val text: LiveData<String> = _text
+
 }
