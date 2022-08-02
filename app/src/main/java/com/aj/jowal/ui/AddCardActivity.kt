21b1f95785eb933f277b5a4f7d4950f71ec0c424
ali johar <johar.ali1@gmail.com>
2022-08-02 12:18:41 +0430

    Add pattern to editText

--- app/src/main/java/com/aj/jowal/ui/AddCardActivity.kt
+++ app/src/main/java/com/aj/jowal/ui/AddCardActivity.kt
@@ -1,12 +1,13 @@
 package com.aj.jowal.ui
 
-import android.R
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import android.text.InputFilter
+import android.text.InputFilter.AllCaps
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import com.aj.jowal.databinding.ActivityAddCardBinding
+import com.szagurskii.patternedtextwatcher.PatternedTextWatcher
 import kotlinx.android.synthetic.main.toolbar.*
 
 
@@ -21,6 +22,12 @@ class AddCardActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[AddCardViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        // Format four digits to a mask of a credit card number.
+        // Format four digits to a mask of a credit card number.
+
+        binding.cardNumberEdittext.addTextChangedListener(PatternedTextWatcher("####-####-####-####"))
+        binding.expireDateEdittext.addTextChangedListener(PatternedTextWatcher("##/##"))
+        binding.nameOnCardEdittext.filters = arrayOf<InputFilter>(AllCaps())
         setContentView(binding.root)
 
     }
